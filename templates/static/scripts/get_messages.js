89f55089204621f9543a08f04275a0765fda7e57
Exr0n <spotyie@gmail.com>
2019-04-17 16:49:20 -0700

    get_messags.js prelim code

--- templates/static/scripts/get_messages.js
+++ templates/static/scripts/get_messages.js
@@ -1,9 +1,27 @@
+var message_list, update_interval;
+
 function getMessages() {
-  
+  var msgReq = new XMLHttpRequest();
+  msgReq.onreadystatechange = () => {
+    if (msgReq.readyState == 4 && msgReq.status == 200) {
+      updateDisplay(JSON.parse(msgReq.responseText));
+    }
+  }
+  msgReq.open("POST", "/sys/update");
+  msgReq.send();
 }
 
-function updateDisplay() {
-
+function updateDisplay(message) {
+  message_list.innerHTML += `
+  <li>
+    <span class="message-author">${message.author}</span>
+    <span class="message-timestamp">${message.timestamp}</span>
+    <span class="message-contents">${message.contents}</span>
+  </li>
+  `;
 }
 
-var update_interval = setInterval(getMessages, config.im_behavior.update_interval); // twice a second
+window.onload = () => {
+  message_list = document.getElementById('message-list');
+  update_interval = setInterval(getMessages, config.im_behavior.update_interval); // twice a second
+}
