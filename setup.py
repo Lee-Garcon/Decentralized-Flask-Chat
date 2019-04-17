46f39bc1d489775071deba3cfb5b11b915f8df96
Lee-Garcon <42008384+Lee-Garcon@users.noreply.github.com>
2019-04-17 15:22:49 -0700

    Made setup.py

---
+++ setup.py
@@ -0,0 +1,66 @@
+import io
+import os
+import sys
+from shutil import rmtree
+from setuptools import find_packages, setup, Command
+
+NAME = 'Decentralized-HyperChat-Program'
+DESCRIPTION = 'Chat Client for Peer-to-Peer Connections. abbr: DHCP'
+URL = 'https://github.com/Lee-Garcon/Decentralized-Flask-Chat'
+EMAIL = 'wallace.dylan00@gmail.com'
+AUTHOR = 'Lee-Garcon, Exr0n'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = 0.0.1
+
+REQUIRED = ['pycrypto', 'flask', 'flask-cors']
+
+cpath = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(cpath, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(cpath, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
