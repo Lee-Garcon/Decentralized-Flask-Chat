1a10a74b5f97fa6310e11c582fd8047d1e99c26e
Exr0n <spotyie@gmail.com>
2019-04-17 16:56:46 -0700

    preliminary send_message code

--- templates/static/scripts/send_messages.js
+++ templates/static/scripts/send_messages.js
@@ -1,3 +1,14 @@
 function sendMessage(text) {
-  
+  var errTimeout = setTimeout(() => {
+    alert('Something went wrong whilst sending the message, is the server down?');
+  }, config.im_behavior.send_server_timeout);
+
+  var sendReq = new XMLHttpRequest();
+  sendReq.onreadystatechange = () => {
+    if (sendReq.readyState == 4 && sendReq.status == 200) {
+      clearTimeout(errTimeout); // stop the client from yelling about stuff
+    }
+  }
+  sendReq.open("POST", "/sys/send_message");
+  sendReq.send();
 }
