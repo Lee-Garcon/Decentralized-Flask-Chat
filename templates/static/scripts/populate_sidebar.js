bab59db6b69de483200c50294f7c260aa5a6a70e
Exr0n <spotyie@gmail.com>
2019-04-18 15:10:55 -0700

    css changes

--- templates/static/scripts/populate_sidebar.js
+++ templates/static/scripts/populate_sidebar.js
@@ -5,13 +5,20 @@ function populateSidebar() {
       let contacts = JSON.stringify(req.responseText);
 
       for (let entry of contacts) {
-        populateSidebar.sidebar.innerHTML += `
+        /*populateSidebar.sidebar.innerHTML += `
         <li class="sidebar-entry${(entry.id == config.sysenv.curr_user ? " sidebar-entry-selected" : "")}">
           <button href="/chat/${entry.id}">
             <p>${entry.name}</p>
           </button>
         </li>
-        `;
+        `;*/
+
+        populateSidebar.sidebar.innerHTML += `
+        <li class="sidebar-entry${(entry.id == config.sysenv.curr_user ? " sidebar-entry-selected" : "")}">
+          <div>
+            <a href="/chat/${entry.id}">${entry.name}</a>
+          </div>
+        </li>`;
       }
     }
   }
